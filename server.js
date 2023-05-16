a49b4c6636bea874f32ff85f48c7a50dc4a45624
Muhammad Usman <contact@mhmd.us>
2023-05-15 17:57:10 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require("fs");
+const { createCanvas } = require("canvas");
 
-app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
-  name = req.params["name"];
-  if (name) {
-    res.send(greeting + "</br>and hello to " + name);
-  } else {
-    res.send(greeting);
-  }
+app.get(["/"], (req, res) => {
+  greeting =
+    "<h3>Get a placeholder image using `width x height` in the URL</h3>";
+  res.send(greeting);
 });
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+app.get("/:size", (req, res) => {
+  const size = getSize(req.params["size"]);
+  console.log({ size });
+  if (size[0] > 10000 || size[1] > 10000) {
+    res.send("Requested image too large");
+    return;
+  }
 
+  // res.send(`<p>${size}</p>`);
+  const i = getImage(size);
+  console.log(i);
+  res.contentType("image/jpeg");
+  res.send(i);
+});
+
+app.listen(port, () =>
+  console.log(`Placeholder app listening on port ${port}!`)
+);
+
+// returns a size of either [width, height]
+function getSize(params) {
+  if (params.includes("x")) {
+    const splitSize = params.split("x");
+    return [parseInt(splitSize[0]), parseInt(splitSize[1])];
+  } else {
+    return [parseInt(params), parseInt(params)];
+  }
+}
+
+function getImage(size) {
+  const WIDTH = size[0];
+  const HEIGHT = size[1];
+
+  const canvas = createCanvas(WIDTH, HEIGHT);
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "#CCCCCC";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillStyle = "#2A1863";
+  ctx.font = "32px Arial";
+  ctx.fillText(`${WIDTH} x ${HEIGHT}`, 16, 32);
+
+  const buffer = canvas.toBuffer("image/jpeg");
+  fs.writeFileSync("test.jpeg", buffer);
+  return buffer;
+}
